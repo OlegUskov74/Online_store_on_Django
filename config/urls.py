0d23da7b3224b2a0fd656b21742d935add9535cb
Олег Усков <oleg81uskov@gmail.com>
2025-06-19 22:15:30 +0500

    init project

---
+++ config/urls.py
@@ -0,0 +1,23 @@
+"""
+Конфигурация URL для проекта конфигурацииКонфигурация URL для проекта конфигурации.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Примеры:
+Виды функций
+    1. Добавьте импорт: from my_app import views
+    2. Добавьте URL в urlpatterns: path('', views.home, name='home')
+Виды на основе классов
+    1. Добавьте импорт: from other_app.views import Home
+    2. Добавьте URL в urlpatterns: path('', Home.as_view(), name='home')
+Включение другого URLconf
+    1. Импортируйте функцию include(): from django.urls import include, path
+    2. Добавьте URL в urlpatterns: path('blog/', include('blog.urls'))
+"""
+
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+]
