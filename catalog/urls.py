0f61a5899013c706d3e2e6985134f8bd32c6dd2e
Олег Усков <oleg81uskov@gmail.com>
2025-07-16 17:23:30 +0500

    homework is ready

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import path
 from catalog.apps import CatalogConfig
 from .views import HomeView, ContactsView, ProductPayView, ProductListView, ProductDetailView
 
